1ccb365ea3a2899c2d333ebc0d119abbe1abc7b6
SkyeMozScriptz <SkyeMozOfficial@gmail.com>
2025-08-24 15:59:19 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function playClick() {
   const audio = new Audio('sounds/click.ogg');
   audio.play();
 }
+window.addEventListener('load', () => {
+  const music = new Audio('sounds/bg-music.mp3');
+  music.loop = true;
+  music.volume = 0.3;
+  music.play().catch(e => {
+    console.log("Autoplay blocked, user needs to interact with the page first");
+  });
+});
